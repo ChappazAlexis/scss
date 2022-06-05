5e40054aeb4bbd3882a66677deee23dc17203443
ChappazAlexis <83456592+ChappazAlexis@users.noreply.github.com>
2022-06-05 12:27:19 +0200

    Add PHP

--- header.php
+++ header.php
@@ -1,15 +1,4 @@
 <header>
-    <?php
-    $url = "http://127.0.0.1/scss-main/index.php";
-    $key = "index";
-
-    if (strpos($url, $key) == false) {
-        
-    }
-
-    ?>
-
-
-    <a class="active" href="./index.php">Accueil</a>
-    <a class="inactive" href="./article.php">Article</a>
+        <a href="./index.php">Accueil</a>
+        <a href="./article.php">Article</a>
     </header>
