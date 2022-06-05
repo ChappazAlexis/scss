3cd0870c9d18f8648d192158cf65b8c2646b5d7c
ChappazAlexis <83456592+ChappazAlexis@users.noreply.github.com>
2022-06-05 12:26:48 +0200

    Add PHP

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <?php
+    $url = "http://127.0.0.1/scss-main/index.php";
+    $key = "index";
+
+    if (strpos($url, $key) == false) {
+        
+    }
+
+    ?>
+
+
+    <a class="active" href="./index.php">Accueil</a>
+    <a class="inactive" href="./article.php">Article</a>
+    </header>
---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./dist/style.css">
+</head>
+
+<? include './header.php'; ?>
+
+<body>
+    <main>
+        <div id="content">
+
+            <div class="contentClass">
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/titanic.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>TITANIC</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/parrain.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>LE PARRAIN</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/gladiator.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>GLADIATOR</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/indiana.png" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>INDIANA JONES et les aventuriers de l'arche perdu</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/asterix.jpeg" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>ASTERIX et le secret de la potion magique</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="imgCard">
+                        <a href="./article.html">
+                            <img src="./img/chtis.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="titleCard">
+                        <h3>BIENVENUE CHEZ LES CH'TIS</h3>
+                    </div>
+                    <div class="descCard">
+                        <p>Synopsis : Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quis assumenda velit qui, nobis cupiditate deleniti consectetur itaque magni minima nihil labore amet eum perferendis eaque. Quidem quibusdam labore numquam?</p>
+                    </div>
+                </div>
+            </div>
+            <aside>
+                <div class="imgAside">
+                    <img src="./img/adblock.png" alt="">
+                </div>
+
+                <div class="asideContent">
+                    <div class="articles">
+                        <ul>
+                            <h2>Articles</h2>
+                            <li>Meilleurs films 2010-2020</li>
+                            <li>Meilleurs films 2000-2010</li>
+                            <li>Meilleurs films 1990-2000</li>
+                        </ul>
+                    </div>
+                    <div class="categories">
+                        <ul>
+                            <h2>Catégories</h2>
+                            <li>Comédie</li>
+                            <li>Action / Aventure</li>
+                            <li>Mafia</li>
+                        </ul>
+                    </div>
+
+                    <div class="contact">
+                        <h2>Contact</h2>
+                        <p>6 Place Charles Hernu</p>
+                        <p>69100, Villeurbanne</p>
+                        <p>mail@mail.com</p>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </main>
+    <? include './footer.php'; ?>
+</body>
+
+</html>
